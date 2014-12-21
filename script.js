93a689d18187a2dfd224f71478c400b46442fdcb
nobe4 <haffreinguev@gmail.com>
2014-12-21 21:59:51 +0100

    added pause button and some style/display refactoring

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 $(function() {
-	var index = 0;
-	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=plane&tagmode=any&format=json&jsoncallback=?",
-						function(data){
-							var images = [];
-							for(var index in data.items){
-								var url = data.items[index].media.m.replace("_m","");
-								images.push(url);
-							}
-							$.backstretch(images,{duration : 50000, fade:5000});
-						});
+
+	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=plane&tagmode=any&format=json&jsoncallback=?", handleImages);
+
+	function handleImages(data){
+		var images = [];
+		for(var index in data.items){
+			var url = data.items[index].media.m.replace("_m","");
+			images.push(url);
+		}
+		$.backstretch(images,{duration : 50000, fade:5000});
+	}
+
+	$("#pause").click(function(){
+		$("#atc-audio")[0].pause();
+		$("#music-audio")[0].pause();
+	});
 });
