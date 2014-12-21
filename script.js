b721773304ea64d648471cabb763f4faf98ded79
nobe4 <haffreinguev@gmail.com>
2014-12-21 21:31:17 +0100

    local music and audio only

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(function() {
-	$.backstretch("http://farm8.staticflickr.com/7517/15884137398_5fd8ac47c6.jpg");
 	var index = 0;
 	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=plane&tagmode=any&format=json&jsoncallback=?",
 						function(data){
